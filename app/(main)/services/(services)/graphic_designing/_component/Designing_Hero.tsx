c5ce2f8d2f747c9dd95782849b4aa44c25404267
fardeen awais <fardeenjutt99@gmail.com>
2024-02-07 16:24:44 +0500

    Hero section updated

--- app/(main)/services/(services)/graphic_designing/_component/Designing_Hero.tsx
+++ app/(main)/services/(services)/graphic_designing/_component/Designing_Hero.tsx
@@ -8,7 +8,7 @@ const Designing_Hero = ({ title, description }: any) => {
         <div className='flex flex-col sm:flex-row items-center justify-center gap-0 mx-auto max-w-7xl px-5 pt-12'>
                 <div className='flex flex-col items-center sm:items-start justify-start gap-5 '>
                     <div className='flex flex-col items-center sm:items-start justify-center'>
-                        <h1 className={`text-3xl md:text-4xl lg:text-5xl font-bold bg-gradient-to-r from-black via-cyan-400 to-cyan-600 text-transparent bg-clip-text py-3`}>{title}</h1>
+                        <h1 className={`text-3xl md:text-4xl lg:text-5xl font-bold bg-gradient-to-r from-black via-cyan-400 to-cyan-600 sm:text-start text-center text-transparent bg-clip-text py-3`}>{title}</h1>
                         <p className='text-black max-w-2xl md:text-xl text-center sm:text-start px-1'>{description}</p>
                     </div>
                     <TalkNowBtn />
