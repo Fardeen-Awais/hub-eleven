c252c0ef78f7c21d3c156278485147fab2b4e706
fardeen awais <fardeenjutt99@gmail.com>
2024-03-30 00:15:09 +0500

    case study - Layout change of case study page

--- app/(main)/work/page.tsx
+++ app/(main)/work/page.tsx
@@ -20,7 +20,7 @@ const page = async () => {
       <div className='flex flex-col items-center gap-5 p-2'>
         <h3 className='max-w-2xl text-center font-semibold'><span className='text-cyan-600'>Case studies.</span>See what our top customer says about us</h3>
       </div>
-      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10'>
+      <div className='flex flex-col gap-20 sm:gap-40'>
         {caseStudies.map((caseStudy: any) => (
           <CaseStudy key={caseStudy._id} industry={caseStudy.industry} title={caseStudy.title} description={caseStudy.desc} label='View Case Study' image={caseStudy.mainImage} href={`/work/${caseStudy.slug.current}`} />
         ))}
--- app/components/shared/case-study.tsx
+++ app/components/shared/case-study.tsx
@@ -1,12 +1,10 @@
 import React from 'react'
-import { Card, CardHeader, CardFooter, CardContent } from '@/components/ui/card';
-import { Image } from '@nextui-org/react';
-import NextImage from "next/image"
-import RedirectBtn from '../RedirectButton';
-import { Separator } from '@/components/ui/separator';
 import imageUrlBuilder from '@sanity/image-url'
 import { client } from '@/sanity/lib/client';
-
+import { Separator } from '@/components/ui/separator';
+import { Image } from '@nextui-org/react';
+import NextImage from 'next/image'
+import Link from 'next/link';
 
 interface CardWrapperProps {
     description: string;
@@ -17,37 +15,28 @@ interface CardWrapperProps {
     image: string
 }
 
-const CaseStudy = ({ industry, title, description, label, image,href }: CardWrapperProps) => {
+const CaseStudy = ({ industry, title, description, label, image, href }: CardWrapperProps) => {
     const builder = imageUrlBuilder(client)
 
-    function urlFor(source:any) {
+    function urlFor(source: any) {
         return builder.image(source)
     }
     return (
-        <Card className='w-80 sm:w-72 lg:w-96 shadow-lg rounded-lg '>
-            <Image className='h-72 w-full' as={NextImage} radius='none' src={urlFor(image).width(500).url()} alt={title} width={900} height={900} sizes='' />
-            <CardHeader>
+        <div className='flex flex-col sm:flex-row items-center justify-between gap-10'>
+            <div className='flex flex-col gap-3'>
+                <div className='flex flex-col'>
+                    <h3 className='font-bold'>{title}</h3>
+                    <p>{industry}</p>
+                </div>
                 <div>
-                    <p><span className='text-lg font-semibold'>Industry</span> | {industry}</p>
+                    <Separator />
+                    <p className='max-w-xl'>{description} Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum rerum repellendus facere, rem hic tenetur accusantium mollitia iure neque temporibus nobis veniam quidem sequi magnam deserunt repudiandae tempora similique inventore a? Hic doloremque consequuntur ex consequatur ut laborum animi explicabo?</p>
                 </div>
-            </CardHeader>
-            <Separator className='my-3' />
-            <CardContent>
-                <div className='flex flex-col gap-5 '>
-                    <div>
-                        <h4 className='font-semibold text-2xl line-clamp-1'>{title}</h4>
-                    </div>
-                    <div>
-                        <p className='line-clamp-5'>{description}</p>
-                    </div>
+                <Link href={href} className='text-lg font-semibold text-cyan-600 hover:text-red-500 transition-all'>{label}</Link>
+            </div>
+            <Image className='h-72 w-full' as={NextImage} radius='none' src={urlFor(image).width(2000).url()} alt={title} width={2000} height={2000} sizes='' />
 
-                </div>
-            </CardContent>
-
-            <CardFooter>
-                <RedirectBtn label={label} href={href} target="" size="default" />
-            </CardFooter>
-        </Card>
+        </div>
     )
 }
 
